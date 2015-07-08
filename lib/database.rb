355b013cd38649beb56c52a2e3c7ddef18a1bbb8
Rafal Dittwald <rafal.dittwald@gmail.com>
2015-07-08 11:05:13 -0400

    moved Database into its own file

---
+++ lib/database.rb
@@ -0,0 +1,32 @@
+require 'csv'
+
+class Database
+  def initialize(db_file)
+    @db_file = db_file
+  end
+
+  def all
+    CSV.read(@db_file)
+  end
+
+  def find
+  end
+
+  def add(arr)
+    CSV.open(@db_file,"ab") do |csv|
+      csv << arr
+    end
+  end
+
+  def count
+    CSV.read(@db_file).length
+  end
+
+  def overwrite_all(tracks)
+    CSV.open(@db_file, "wb") do |csv|
+      tracks.each do |t|
+        csv << t
+      end
+    end
+  end
+end
--- lib/music_database.rb
+++ lib/music_database.rb
@@ -1,35 +1,4 @@
-require 'csv'
-
-class Database
-  def initialize(db_file)
-    @db_file = db_file
-  end
-
-  def all
-    CSV.read(@db_file)
-  end
-
-  def find
-  end
-
-  def add(arr)
-    CSV.open(@db_file,"ab") do |csv|
-      csv << arr
-    end
-  end
-
-  def count
-    CSV.read(@db_file).length
-  end
-
-  def overwrite_all(tracks)
-    CSV.open(@db_file, "wb") do |csv|
-      tracks.each do |t|
-        csv << t
-      end
-    end
-  end
-end
+require_relative 'database'
 
 def run(output, input_stream, db_file)
   output.puts "Welcome to Mango Music"
