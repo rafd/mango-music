bcaa4eefadf84c56c8bbc3bbd7532c374de9a836
Rafal Dittwald <rafal.dittwald@gmail.com>
2015-07-08 10:14:58 -0400

    testing adding

--- spec/interface_spec.rb
+++ spec/interface_spec.rb
@@ -23,5 +23,15 @@ describe "interface" do
       app = run(output,input)
     end
   end
+  describe "add" do
+    it "adds a track" do
+      output = double(:output).as_null_object
+      output.should_receive(:puts).with(/[0-9]+: aaa by bbb \(0 listens\)/)
+
+      input = StringIO.new("add aaa bbb\nlist\nexit\n")
+
+      app = run(output,input)
+    end
+  end
 end
 
