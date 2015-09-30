039a09dc04fc32dfe3e53ba5799be49e20f423cf
Rafal Dittwald <rafal.dittwald@gmail.com>
2015-09-30 09:59:56 -0400

    moved CSV methods into Database class

--- lib/music_database.rb
+++ lib/music_database.rb
@@ -1,5 +1,33 @@
 require 'csv'
 
+class Database
+  def initialize(db_file)
+    @db_file = db_file
+  end
+
+  def count
+    CSV.read(@db_file).length
+  end
+
+  def add(data)
+    CSV.open(@db_file,"ab") do |csv|
+      csv << data
+    end
+  end
+
+  def all
+    CSV.read(@db_file)
+  end
+
+  def replace(data)
+    CSV.open(@db_file, "wb") do |csv|
+      data.each do |t|
+        csv << t
+      end
+    end
+  end
+end
+
 def run(output, input_stream, db_file)
   output.puts "Welcome to Mango Music"
   output.puts "You can:"
@@ -11,18 +39,17 @@ def run(output, input_stream, db_file)
   output.puts ""
   output.puts "What do now?"
   input = input_stream.gets.chomp
+  db = Database.new(db_file)
 
   while input != "exit"
     case input.split(" ")[0]
     when "add"
-      track_count = CSV.read(db_file).length
-      CSV.open(db_file,"ab") do |csv|
-        csv << [track_count, input.split(" ")[1], input.split(" ")[2], 0]
-      end
+      track_count = db.count
+      db.add([track_count, input.split(" ")[1], input.split(" ")[2], 0])
       output.puts "saved!"
     when "listen"
       title = input.split(" ")[1]
-      tracks = CSV.read(db_file)
+      tracks = db.all
       track = tracks.select do |t|
          t[1] == title
       end
@@ -30,20 +57,16 @@ def run(output, input_stream, db_file)
       if track
         output.puts "You're listening to... #{track[1]} by #{track[2]}"
         tracks[track[0].to_i][3] = tracks[track[0].to_i][3].to_i + 1
-        CSV.open(db_file, "wb") do |csv|
-          tracks.each do |t|
-            csv << t
-          end
-        end
+        db.replace(tracks)
       end
     when "list"
-      tracks = CSV.read(db_file)
+      tracks = db.all
       tracks.each do |t|
         output.puts "#{t[0]}: #{t[1]} by #{t[2]} (#{t[3]} listens)"
       end
     when "search"
       tracks = []
-      CSV.read(db_file).each do |track|
+      db.all.each do |track|
         if /#{input.split(" ")[1]}/ =~ track[1]
           tracks << track
         elsif /#{input.split(" ")[1]}/ =~ track[2]
